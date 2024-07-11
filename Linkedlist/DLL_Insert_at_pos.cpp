43cfe10ced245867f1d2842d04ef2c724c6df195
Sandhya_Giridharan <146985875+Sandhya-3024@users.noreply.github.com>
2024-07-11 16:26:32 +0530

    Update DLL_Insert_at_pos.cpp

--- Linkedlist/DLL_Insert_at_pos.cpp
+++ Linkedlist/DLL_Insert_at_pos.cpp
@@ -12,3 +12,36 @@ void addNode(Node *head, int pos, int data)
    newnode->next=temp->next;
    temp->next=newnode;
 }
+//0-based indexing
+ Node* insert(int k, int val, Node *head) {
+    Node* newNode = new Node(val);
+    if(head==NULL){
+        return NULL;
+    }
+
+    if (head==NULL && k == 0) {
+        newNode->next = head;
+        head->prev = newNode;
+        return newNode;
+    }
+    if(k==0){
+        newNode->next=head;
+        newNode->prev=NULL;
+        return newNode;
+    }
+    int cnt=0;
+    Node* temp = head;
+    while (temp != NULL) {
+        cnt++;
+        if(cnt==k){
+            newNode->next=temp->next;
+            temp->next=newNode;
+            newNode->prev=temp;
+            break;
+        }
+        temp = temp->next;
+    } 
+    return head;
+}
+ 
+
