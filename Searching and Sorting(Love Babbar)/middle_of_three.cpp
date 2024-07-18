9484b19af98762e0d5fd8c7bdb3fea595fea037b
Sandhya_Giridharan <146985875+Sandhya-3024@users.noreply.github.com>
2024-07-18 12:47:18 +0530

    Create middle_of_three.cpp

---
+++ Searching and Sorting(Love Babbar)/middle_of_three.cpp
@@ -0,0 +1,17 @@
+int middle(int A, int B, int C){
+      vector<int>arr{A,B,C};
+      int largest=-1;
+      int second_largest=-1;
+      for(int i=0;i<3;i++){
+          if(arr[i]>largest){
+              second_largest=largest;
+              largest=arr[i];
+              
+          }
+          else if(arr[i]<largest && arr[i]>second_largest){
+              second_largest=arr[i];
+          }
+      }
+     return second_largest;
+    }
+    
