ef8917e5bfc27cd93e153129fa6124f9085b4afc
Sandhya_Giridharan <146985875+Sandhya-3024@users.noreply.github.com>
2024-07-23 12:27:25 +0530

    Create Detect_Cycle.cpp

---
+++ Graphs/Detect_Cycle.cpp
@@ -0,0 +1,89 @@
+ //{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    
+    bool detect(vector<int> adj[],int vis[],int node,int parent)
+    {
+        vis[node]=1;
+       for(auto it:adj[node]){
+           if(!vis[it]){
+            if(detect(adj,vis,it,node)) return true;
+           }
+           else if(parent != it){
+               return true;
+           }
+       }
+       return false;
+    }
+        //BFS
+        vis[src]=1;
+        queue<pair<int,int>>q;
+        q.push({src,-1});
+        while(!q.empty()){
+            int node=q.front().first;
+            int parent=q.front().second;
+            q.pop();
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push({it,node});
+                }
+                else if(parent!=it){
+                    return true;
+                }
+            }
+        }
+        return false;*/
+    
+    
+    bool isCycle(int V, vector<int> adj[]) {
+         
+        int vis[V]={0};
+         for(int i=0 ;i<V;i++){
+             if(!vis[i]){
+                 if(detect(adj,vis,i)) return true;
+             }
+         }
+         return false;
+        
+    }
+    
+    int vis[V]={0};
+    for(int i=0;i<V;i++){
+    if(!vis[i]){
+        if(detect(adj,vis,i,-1)==true) return true;
+    }
+    }
+    return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
